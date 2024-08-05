d890c1349e62334926167f6d1cb7e6ae38432848
Govoridaa <mikhail.pasichnike@gmail.com>
2024-08-05 22:53:47 +0300

    Test

--- game.py
+++ game.py
@@ -1,14 +1,19 @@
 import numpy as np
-
+# Переменная счетчик попыток
 count = 0
+# Переменная с рандомным числом в задонном диапазоне
 number = np.random.randint(1, 101)
 print("Загадано число от 1 до 100")
+# Переменная с минимальным порогом 
 min = 0
+# Переменная с максимальным порогом 
 max = 100
+# Создаем бесконечный цикл пока верно
 while True:
     predict = round((min+max)/2)
     #predict = int(input())
     count += 1
+    # Условие выхода из цикла
     if number == predict:
         break
     elif number > predict:
